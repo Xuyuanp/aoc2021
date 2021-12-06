db4f921703119c7704822013eb02b0fbef3bd649
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-06 17:37:11 +0800

    updated day06

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 
-pub fn part1(input: &Vec<String>) -> bool {
-    let mut timers: BTreeMap<u32, u64> = input[0]
+fn lanternfish(init: &Vec<String>, days: u32) -> u64 {
+    let mut timers: BTreeMap<u32, u64> = init[0]
         .split(",")
         .map(|s| s.parse::<u32>().unwrap())
         .fold(BTreeMap::new(), |mut acc, age| {
@@ -9,7 +9,7 @@ pub fn part1(input: &Vec<String>) -> bool {
             acc.insert(age, cnt + 1);
             acc
         });
-    for day in 1..80 {
+    for day in 1..days {
         if timers.keys().nth(0) == Some(&day) {
             let cnt = timers.remove(&day).unwrap();
 
@@ -21,29 +21,15 @@ pub fn part1(input: &Vec<String>) -> bool {
         }
     }
     let total = timers.iter().map(|ent| ent.1).sum::<u64>();
+    total
+}
+
+pub fn part1(input: &Vec<String>) -> bool {
+    let total = lanternfish(input, 80);
     total == 365862
 }
 
 pub fn part2(input: &Vec<String>) -> bool {
-    let mut timers: BTreeMap<u32, u64> = input[0]
-        .split(",")
-        .map(|s| s.parse::<u32>().unwrap())
-        .fold(BTreeMap::new(), |mut acc, age| {
-            let cnt = acc.remove(&age).unwrap_or(0);
-            acc.insert(age, cnt + 1);
-            acc
-        });
-    for day in 1..256 {
-        if timers.keys().nth(0) == Some(&day) {
-            let cnt = timers.remove(&day).unwrap();
-
-            let cnt1 = timers.remove(&(day + 6 + 1)).unwrap_or(0);
-            timers.insert(day + 6 + 1, cnt + cnt1);
-
-            let cnt2 = timers.remove(&(day + 8 + 1)).unwrap_or(0);
-            timers.insert(day + 8 + 1, cnt + cnt2);
-        }
-    }
-    let total = timers.iter().map(|ent| ent.1).sum::<u64>();
+    let total = lanternfish(input, 256);
     total == 1653250886439
 }
