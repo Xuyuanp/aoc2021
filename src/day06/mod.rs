57d06d29131808058fc96614c2bf84160499dcb6
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-06 14:34:41 +0800

    day06 part2

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,20 +1,49 @@
-use std::collections::BinaryHeap;
+use std::collections::BTreeMap;
 
 pub fn part1(input: &Vec<String>) -> bool {
-    let mut timers: BinaryHeap<i32> = input[0]
+    let mut timers: BTreeMap<u32, u64> = input[0]
         .split(",")
-        .map(|s| -s.parse::<i32>().unwrap())
-        .collect();
+        .map(|s| s.parse::<u32>().unwrap())
+        .fold(BTreeMap::new(), |mut acc, age| {
+            let cnt = acc.remove(&age).unwrap_or(0);
+            acc.insert(age, cnt + 1);
+            acc
+        });
     for day in 1..80 {
-        while timers.peek() == Some(&(-&day)) {
-            timers.pop();
-            timers.push(-day - 6 - 1);
-            timers.push(-day - 8 - 1);
+        if timers.keys().nth(0) == Some(&day) {
+            let cnt = timers.remove(&day).unwrap();
+
+            let cnt1 = timers.remove(&(day + 6 + 1)).unwrap_or(0);
+            timers.insert(day + 6 + 1, cnt + cnt1);
+
+            let cnt2 = timers.remove(&(day + 8 + 1)).unwrap_or(0);
+            timers.insert(day + 8 + 1, cnt + cnt2);
         }
     }
-    timers.len() == 365862
+    let total = timers.iter().map(|ent| ent.1).sum::<u64>();
+    total == 365862
 }
 
-pub fn part2(_input: &Vec<String>) -> bool {
-    false
+pub fn part2(input: &Vec<String>) -> bool {
+    let mut timers: BTreeMap<u32, u64> = input[0]
+        .split(",")
+        .map(|s| s.parse::<u32>().unwrap())
+        .fold(BTreeMap::new(), |mut acc, age| {
+            let cnt = acc.remove(&age).unwrap_or(0);
+            acc.insert(age, cnt + 1);
+            acc
+        });
+    for day in 1..256 {
+        if timers.keys().nth(0) == Some(&day) {
+            let cnt = timers.remove(&day).unwrap();
+
+            let cnt1 = timers.remove(&(day + 6 + 1)).unwrap_or(0);
+            timers.insert(day + 6 + 1, cnt + cnt1);
+
+            let cnt2 = timers.remove(&(day + 8 + 1)).unwrap_or(0);
+            timers.insert(day + 8 + 1, cnt + cnt2);
+        }
+    }
+    let total = timers.iter().map(|ent| ent.1).sum::<u64>();
+    total == 1653250886439
 }
