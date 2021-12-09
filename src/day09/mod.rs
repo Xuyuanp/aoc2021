d6af2f44367bcadbafd2f70e4016c0862ccd7531
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-09 19:37:58 +0800

    day09 part1

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -8,27 +8,28 @@ pub fn part1(input: &Vec<String>) -> bool {
         .try_into()
         .unwrap();
 
-    let mut res = 0;
     let base = '0';
-    for i in 0..100 as i32 {
-        for j in 0..100 {
-            let curr = matrix[i as usize][j as usize];
+    let res = (0..100)
+        .map(move |i| (0..100).map(move |j| (i, j)))
+        .flatten()
+        .filter(|(i, j)| {
+            let curr = matrix[*i as usize][*j as usize];
 
-            let is_low_point = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            [(0, 1), (0, -1), (1, 0), (-1, 0)]
                 .iter()
                 .map(|(dx, dy)| (i + dx, j + dy))
                 .filter(|(x, y)| x >= &0 && x < &100 && y >= &0 && y < &100)
                 .map(|(x, y)| matrix[x as usize][y as usize])
                 .filter(|v| v <= &curr)
                 .count()
-                == 0;
+                == 0
+        })
+        .map(|(i, j)| {
+            let curr = matrix[i as usize][j as usize];
+            (curr as u32 - base as u32) + 1
+        })
+        .sum::<u32>();
 
-            if is_low_point {
-                res += (curr as u32 - base as u32) + 1;
-                println!("matrix[{}][{}] = {}", i, j, curr);
-            }
-        }
-    }
     res == 633
 }
 
