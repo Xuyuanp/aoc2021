1f65b89ce3e9365676136c4fe09452b260a2e863
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-14 22:46:00 +0800

    day14 part2

--- src/day14/mod.rs
+++ src/day14/mod.rs
@@ -1,51 +1,62 @@
 use std::collections::HashMap;
 
 fn parse_input(input: &Vec<String>) -> (String, HashMap<String, char>) {
-    let mut map = HashMap::new();
+    let mut rules = HashMap::new();
 
     let templ = input.iter().next().unwrap().to_string();
 
     input[2..].iter().for_each(|line| {
         let seps: Vec<&str> = line.split(" -> ").collect();
-        map.insert(seps[0].to_string(), seps[1].chars().next().unwrap());
+        rules.insert(seps[0].to_string(), seps[1].chars().next().unwrap());
     });
 
-    (templ, map)
+    (templ, rules)
+}
+
+fn run_steps(templ: String, rules: HashMap<String, char>, steps: usize) -> u64 {
+    let mut letters = HashMap::new();
+    let mut pairs = HashMap::new();
+
+    templ
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(2)
+        .for_each(|chars| {
+            *pairs.entry(chars.iter().collect::<String>()).or_insert(0) += 1;
+            *letters.entry(chars[0]).or_insert(0) += 1;
+        });
+
+    *letters.entry(templ.chars().last().unwrap()).or_insert(0) += 1;
+
+    for _ in 0..steps {
+        let mut new_pairs: HashMap<String, u64> = HashMap::new();
+        for (pair, cnt) in pairs.iter() {
+            let new_char = *rules.get(pair).unwrap();
+            *letters.entry(new_char).or_insert(0) += cnt;
+            *new_pairs
+                .entry([pair.chars().next().unwrap(), new_char].iter().collect())
+                .or_insert(0) += cnt;
+            *new_pairs
+                .entry([new_char, pair.chars().last().unwrap()].iter().collect())
+                .or_insert(0) += cnt;
+        }
+        pairs = new_pairs;
+    }
+
+    letters.values().max().unwrap() - letters.values().min().unwrap()
 }
 
 pub fn part1(input: &Vec<String>) -> bool {
-    let (templ, map) = parse_input(input);
-
-    let mut start: Vec<char> = templ.chars().collect();
-
-    for _i in 0..10 {
-        start = start.windows(2).map(|chars| {
-            let pair: String = chars.iter().collect();
-            [chars[0], *map.get(&pair).unwrap(), chars[1]]
-        }).fold(Vec::new(), |mut vec, chars| {
-            if vec.len() == 0 {
-                vec.extend_from_slice(&chars);
-            } else {
-                vec.extend_from_slice(&chars[1..])
-            }
-            vec
-        }) ;
-    }
-    let counter = start.iter().fold(HashMap::new(), |mut counter, ch| {
-        *counter.entry(ch).or_insert(0) += 1;
-        counter
-    });
-
-    let mut freq: Vec<i32> = counter.into_values().collect();
-    let len = freq.len();
-    freq.select_nth_unstable(0);
-    freq.select_nth_unstable(len-1);
-
-    let res = freq[len-1] - freq[0];
+    let (templ, rules) = parse_input(input);
+    let res = run_steps(templ, rules, 10);
 
     res == 2112
 }
 
-pub fn part2(_input: &Vec<String>) -> bool {
-    unimplemented!();
+pub fn part2(input: &Vec<String>) -> bool {
+    let (templ, rules) = parse_input(input);
+
+    let res = run_steps(templ, rules, 40);
+
+    res == 3243771149914
 }
