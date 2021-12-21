0829885587f9a93e3b581c24ec1e543f35e77c9f
Xuyuan Pang <xuyuanp@gmail.com>
2021-12-21 11:39:34 +0800

    update day20

--- src/day20/mod.rs
+++ src/day20/mod.rs
@@ -1,76 +1,113 @@
+use std::collections::HashSet;
 use std::convert::TryInto;
 
-fn enhance(image: &Vec<Vec<char>>, trans: &[char; 512], default: char) -> (Vec<Vec<char>>, char) {
-    let (m, n) = (image.len(), image[0].len());
-    let mut new: Vec<Vec<char>> = Vec::with_capacity(m + 2);
-    for _ in 0..m + 2 {
-        let row = ".".repeat(n + 2).chars().collect();
-        new.push(row);
+#[derive(Debug)]
+struct Image {
+    lights: HashSet<(i32, i32)>,
+    width: usize,
+    height: usize,
+    default_on: bool,
+}
+
+impl Image {
+    fn new(width: usize, height: usize, default_on: bool) -> Self {
+        Self {
+            width,
+            height,
+            default_on,
+            lights: HashSet::new(),
+        }
     }
 
-    let (m, n) = (m as i32, n as i32);
-    (-1..m + 1)
-        .enumerate()
-        .map(move |x| (-1..n + 1).enumerate().map(move |y| (x, y)))
-        .flatten()
-        .for_each(|p| {
-            let ((new_x, old_x), (new_y, old_y)) = p;
-            let idx = (-1..2)
-                .map(move |dx| (-1..2).map(move |dy| (old_x + dx, old_y + dy)))
-                .flatten()
-                .map(|(x, y)| {
-                    if x < 0 || y < 0 || x >= m || y >= n {
-                        default
-                    } else {
-                        image[x as usize][y as usize]
-                    }
-                })
-                .fold(0, |acc, c| {
-                    acc << 1
-                        | (match c {
-                            '.' => 0,
-                            '#' => 1,
-                            _ => unreachable!("unknown pixel: {}", c),
-                        } as usize)
-                });
-            new[new_x][new_y] = trans[idx];
-        });
+    fn from(matrix: &Vec<Vec<char>>) -> Self {
+        let (height, width) = (matrix.len(), matrix[0].len());
+        let mut lights = HashSet::new();
+        for i in 0..height {
+            for j in 0..width {
+                if matrix[i][j] == '#' {
+                    lights.insert((i as i32, j as i32));
+                }
+            }
+        }
+        Self {
+            width,
+            height,
+            lights,
+            default_on: false,
+        }
+    }
 
-    (new, if default == '.' { '#' } else { '.' })
+    fn collect_bits(&self, x: i32, y: i32) -> usize {
+        let (w, h) = (self.width as i32, self.height as i32);
+        (-1..2)
+            .map(move |dx| (-1..2).map(move |dy| (x + dx, y + dy)))
+            .flatten()
+            .map(|(nx, ny)| {
+                if nx < 0 || nx >= h || ny < 0 || ny >= w {
+                    if self.default_on {
+                        1
+                    } else {
+                        0
+                    }
+                } else {
+                    if self.lights.contains(&(nx, ny)) {
+                        1
+                    } else {
+                        0
+                    }
+                }
+            })
+            .fold(0, |acc, b| acc << 1 | b)
+    }
+
+    fn enhance(&self, trans: &[char; 512]) -> Self {
+        let mut new = Self::new(self.width + 2, self.height + 2, !self.default_on);
+
+        let (w, h) = (self.width as i32, self.height as i32);
+        (-1..h + 1)
+            .enumerate()
+            .map(move |x| (-1..w + 1).enumerate().map(move |y| (x, y)))
+            .flatten()
+            .for_each(|p| {
+                let ((new_x, old_x), (new_y, old_y)) = p;
+                let idx = self.collect_bits(old_x, old_y);
+                if trans[idx] == '#' {
+                    new.lights.insert((new_x as i32, new_y as i32));
+                }
+            });
+
+        new
+    }
 }
 
 pub fn part1(input: &Vec<String>) -> bool {
     let trans: [char; 512] = input[0].chars().collect::<Vec<_>>().try_into().unwrap();
-    let mut image: Vec<Vec<char>> = input[2..]
+    let matrix: Vec<Vec<char>> = input[2..]
         .iter()
         .map(|row| row.chars().collect::<Vec<_>>())
         .collect();
+    let mut image = Image::from(&matrix);
 
-    let mut default = '.';
     for _ in 0..2 {
-        let new = enhance(&image, &trans, default);
-        image = new.0;
-        default = new.1;
+        image = image.enhance(&trans);
     }
 
-    let res = image.iter().flatten().filter(|c| c == &&'#').count();
+    let res = image.lights.len();
     res == 5306
 }
 
 pub fn part2(input: &Vec<String>) -> bool {
     let trans: [char; 512] = input[0].chars().collect::<Vec<_>>().try_into().unwrap();
-    let mut image: Vec<Vec<char>> = input[2..]
+    let matrix: Vec<Vec<char>> = input[2..]
         .iter()
         .map(|row| row.chars().collect::<Vec<_>>())
         .collect();
+    let mut image = Image::from(&matrix);
 
-    let mut default = '.';
     for _ in 0..50 {
-        let new = enhance(&image, &trans, default);
-        image = new.0;
-        default = new.1;
+        image = image.enhance(&trans);
     }
 
-    let res = image.iter().flatten().filter(|c| c == &&'#').count();
+    let res = image.lights.len();
     res == 17497
 }
